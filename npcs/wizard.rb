feead09bef6f9c8dc214e63d34c1fafc21ef1a7b
Brendan Crawford <bcgir87@gmail.com>
2014-08-09 12:15:23 -0700

    fixed typo in wizard talk

--- npcs/wizard.rb
+++ npcs/wizard.rb
@@ -13,12 +13,12 @@ class Wizard < NPC
 
 	def talk(hero)
 		if hero.inv.any? { |i| i.names[0] == "Mysterious Black Pearl" }
-			puts "\n\"You have done it! You have felled the Skeleton King! Wait... What's this? The \nBlack Pearl!? It can't be... King Tygain... #{hero.names[0]}, this is an artifact \nleft from a kingdom whos name has been forgotton long before even I was born. \nYou now hold the legacy of the Strongfist Clan. If you can restore the Black Pearl to it's \nrightful place, then the Curse of Cursed King my be lifted.\""
+			puts "\n\"You have done it! You have felled the Skeleton King! Wait... What's this? The \nBlack Pearl!? It can't be... King Tygain... #{hero.names[0]}, this is an artifact \nleft from a kingdom whos name has been forgotton long before even I was born. \nYou now hold the legacy of the Strongfist Clan. If you can restore the Black Pearl \nto it's rightful place, then the Curse of Cursed King my be lifted.\""
 		else
 			puts "\n\"Greetings #{hero.names[0]}, you look like a capable adventurer. If you are \nplanning on delving into the depths of the dungeon, you had better go prepared. \nI think there might be some things around this inn which could help you. And if \nyou die down there at least I can see if that assassin is \nstill around here...\""
 		end
 		Game.pause_short
-		puts "\n\"Is your name ready to written into the great stories?\" (Save? y/n)"
+		puts "\n\"Is your name ready to be written into the great stories?\" \n(Save? y/n)"
 		confirm = gets.chomp.downcase
 		until confirm == "y" || confirm == "n"
 			puts "\nType 'y' or 'n'"
